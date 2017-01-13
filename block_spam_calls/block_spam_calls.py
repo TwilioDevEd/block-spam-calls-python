e81ed61c366be5b661a3066d3cd223297edd386b
Samuel Mendes <samuel.mendes@gmail.com>
2017-01-13 15:17:50 +0100

    Initial Commit

---
+++ block_spam_calls/block_spam_calls.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+import twilio.twiml
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST'])
+def block_spam_calls():
+    resp = twilio.twiml.Response()
+    block_call = False
+
+    blocker_addons = {
+        "nomorobo_spamscore": should_be_blocked_by_nomorobo,
+        "whitepages_pro_phone_rep": should_be_blocked_by_whitepages,
+        "marchex_cleancall": should_be_blocked_by_marchex
+    }
+
+    if 'AddOns' in request.form:
+        add_ons = json.loads(request.form['AddOns'])
+        if add_ons['status'] == 'successful':
+            for blocker_name, blocker_func in blocker_addons.items():
+                should_be_blocked = blocker_func(add_ons['results'].get(blocker_name, {}))
+                # print(f'{blocker_name} should be blocked ? {should_be_blocked}')
+                block_call = block_call or should_be_blocked
+
+    if block_call:
+        resp.reject()
+    else:
+        resp.say("Welcome to the jungle.")
+        resp.hangup()
+    return str(resp)
+
+
+def should_be_blocked_by_nomorobo(nomorobo_spamscore):
+    if nomorobo_spamscore.get('status') != 'successful':
+        return False
+    else:
+        score = nomorobo_spamscore['result'].get('score', 0)
+        return score == 1
+
+
+def should_be_blocked_by_whitepages(whitepages):
+    if whitepages.get('status') != 'successful':
+        return False
+
+    results = whitepages.get('result', {}).get('results', [])
+    for result in results:
+        if result.get('reputation', {}).get('level', 0) == 4:
+            return True
+
+    return False
+
+
+def should_be_blocked_by_marchex(marchex):
+    if marchex.get('status') != 'successful':
+        return False
+
+    recommendation = marchex.get('result', {}).get('result', {}).get('recommendation')
+    return recommendation == 'BLOCK'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ block_spam_calls/block_spam_calls_tests.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import unittest
+import json
+import os
+
+from block_spam_calls import app
+
+
+class BlockSpamCalls(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client(self)
+
+    def load_json_fixture(self, filename):
+        filepath = os.path.join(os.path.dirname(__file__), 'fixtures/', filename)
+        with open(filepath, 'rb') as fd:
+            fixture = fd.read()
+        return fixture.decode('utf-8-sig')
+
+    def test_successful_without_add_ons(self):
+        result = self.app.post('/')
+        self.assertFalse(b'<Reject' in result.data)
+
+    def test_successful_with_marchex(self):
+        add_ons = self.load_json_fixture('successful_marchex.json')
+        result = self.app.post('/', data={'AddOns': add_ons})
+        self.assertFalse(b'<Reject' in result.data)
+
+    def test_blocked_with_marchex(self):
+        add_ons = self.load_json_fixture('spam_marchex.json')
+        result = self.app.post('/', data={'AddOns': add_ons})
+        self.assertTrue(b'<Reject' in result.data)
+
+    def test_successful_with_nomorobo(self):
+        add_ons = self.load_json_fixture('successful_nomorobo.json')
+        result = self.app.post('/', data={'AddOns': add_ons})
+        self.assertFalse(b'<Reject' in result.data)
+
+    def test_blocked_with_nomorobo(self):
+        add_ons = self.load_json_fixture('spam_nomorobo.json')
+        result = self.app.post('/', data={'AddOns': add_ons})
+        self.assertTrue(b'<Reject' in result.data)
+
+    def test_successful_with_whitepages(self):
+        add_ons = self.load_json_fixture('successful_whitepages.json')
+        result = self.app.post('/', data={'AddOns': add_ons})
+        self.assertFalse(b'<Reject' in result.data)
+
+    def test_blocked_with_whitepages(self):
+        add_ons = self.load_json_fixture('spam_whitepages.json')
+        result = self.app.post('/', data={'AddOns': add_ons})
+        self.assertTrue(b'<Reject' in result.data)
+
+    def test_successful_with_nomorobo_api_failure(self):
+        add_ons = self.load_json_fixture('failed_nomorobo.json')
+        result = self.app.post('/', data={'AddOns': add_ons})
+        self.assertFalse(b'<Reject' in result.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
